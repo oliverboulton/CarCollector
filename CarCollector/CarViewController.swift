8b68bd94c8dd1153c5a3413f3d8890a29578b022
Oliver Boulton <olliemarkboulton@gmail.com>
2021-03-29 22:18:27 +0100

    Layout Complete with Car view controlled connected to code

---
+++ CarCollector/CarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CarViewController.swift
+//  CarCollector
+//
+//  Created by Oliver Boulton on 29/03/2021.
+//
+
+import UIKit
+
+class CarViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var carImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func PhotosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
